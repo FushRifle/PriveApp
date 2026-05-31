88f9c77d3a3be4e3263d1f03b337744471de9c2e
FushRifle <ayomikail21@gmail.com>
2026-05-31 23:57:42 +0100

    fix leak

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,6 +11,14 @@ val localProperties =
         rootProject.file("local.properties").inputStream().use { load(it) }
     }
 
+val keystorePropertiesFile = rootProject.file("key.properties")
+val keystoreProperties =
+    Properties().apply {
+        if (keystorePropertiesFile.exists()) {
+            keystorePropertiesFile.inputStream().use { load(it) }
+        }
+    }
+
 android {
     namespace = "com.example.clique"
     compileSdk = flutter.compileSdkVersion
@@ -32,11 +40,23 @@ android {
         versionName = flutter.versionName
     }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String?
+            keyPassword = keystoreProperties["keyPassword"] as String?
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"] as String?
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig =
+                if (keystorePropertiesFile.exists()) {
+                    signingConfigs.getByName("release")
+                } else {
+                    signingConfigs.getByName("debug")
+                }
         }
     }
 }
